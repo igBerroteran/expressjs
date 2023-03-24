585d41a1aa5541417b2aa46965583ba2162b2d47
Igor Berroteran <berroteran.ig@gmail.com>
2023-03-24 11:47:52 -0400

    deploy

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
+
+const app = express();
+const port = process.env.PORT || 3010;
+
+app.use(express.json());
+
+const options = {
+  'Acces-Control-Allow-Origin': ['127.0.0.1:5500/', 'http://localhost:3010/'],
+};
+app.use(cors(options));
+
+app.get('/api', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/api/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+});
